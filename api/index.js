c687be4568d349ff7aff6ac09edb5967d794de7b
Kalle Kauppila <kallekauppila@kamk.fi>
2025-04-07 15:34:04 +0300

    versio 4

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 1</H1>"));
+app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 4</H1>"));
 app.listen(4000, () => console.log("Server ready on port 4000."));
 module.exports = app;
